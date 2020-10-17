ce78edf2b959dd9231298ee44d1e3ac5601194c1
Jhon Sebastian <Sebastamriel@gmail.com>
2020-10-17 09:12:51 -0500

    Configuración de semantic y readme - 17/10/2020, 09:12 a.m

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Container } from 'semantic-ui-react';
+import { Button, Icon } from 'semantic-ui-react';
 
-export function Home () {
+export function Home() {
     return (
-        <h1>Hola mundo</h1>
+        <div>
+            <Container>
+                <h1>Hola mundo</h1>
+                <Button animated='fade'>
+                    <Button.Content visible>Dame clic</Button.Content>
+                    <Button.Content hidden>Tan obediente</Button.Content>
+                </Button>
+            </Container>
+        </div>
     )
 }
