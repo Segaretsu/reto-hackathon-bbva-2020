862e638ef8f07b571034aeedb1f6ab7e874bb3fe
Jhon Sebastian <Sebastamriel@gmail.com>
2020-10-17 16:01:41 -0500

    Se continua con el estado del arbol de decisiones - 17/10/2020, 04:01, p. m.

--- src/constants/arbolDesiciones.js
+++ src/constants/arbolDesiciones.js
@@ -1,8 +1,8 @@
-export function getDesicion() {
-
+export function getDesicion(nodo) {
+    return desiciones[nodo];
 }
 
-export function tomarDesicion() {
+export function tomarDesicion(nodoActual) {
 
 }
 
@@ -10,19 +10,37 @@ export const desiciones = {
     desicionInicial: {
         opcion1: {
             newDesicion: "desicionPadre",
-            descripcion: 'Una de las metas de Joselo es conseguir una casa propia, para esto acepta una oportunidad de trabajo que le dobla su salario actual.'
+            rutaImagen: '',
+            descripcion: 'Una de las metas de José es conseguir una casa propia, para esto acepta una oportunidad de trabajo que le dobla su salario actual',
+            seleccionada: true
         }
     },
 
     desicionPadre: {
         desicionTomada: {
-            descripcion: ""
+            rutaImagen: '',
+            descripcion: 'Para celebrar, sus amigos le dicen que debería salir el proximo fin de semana, ya que es su primer sueldo y debería celebrarlo a lo grande'
         },
         opcion1: {
-            newDesicion: "",
+            newDesicion: '',
+            rutaImagen: '',
+            descripcion: 'José decide no ir',
             seleccionada: false
         },
-        opcion2: {},
-        opcion3: {},
+        opcion2: {
+            newDesicion: '',
+            rutaImagen: '',
+            descripcion: 'José decide que por ahora no puede ir, pero comenta que puede ir en otro momento que todos tengan dinero',
+            retroAlimentacion: {
+                descripcion: 'José sale sin problema con sus amigos el proximo pago',
+            },
+            seleccionada: false
+        },
+        opcion3: {
+            newDesicion: '',
+            rutaImagen: '',
+            descripcion: 'José decide no ir',
+            seleccionada: false
+        },
     }
 };
